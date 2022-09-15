09943b3921aa0ed494d263fcfc9cc1841cd46a91
ginabomination <grugerstorz@gmail.com>
2022-09-16 10:40:45 +1200

    text is algined but it manipluates flex onclick

--- main.js
+++ main.js
@@ -4,14 +4,30 @@
 const img = document.querySelector("#img");
 img.addEventListener("click", reveal);
 
-function reveal() {
-    let hidden = document.getElementById('hidden');
-    if (hidden.style.display == "none") {
-      hidden.style.display = "block"
+function reveal1() {
+    let hidden1 = document.getElementById('hidden1');
+    if (hidden1.style.display == "none") {
+      hidden1.style.display = "block"
     } else {
-      hidden.style.display = "none"
+      hidden1.style.display = "none"
     }
   }
 
+  function reveal2() {
+    let hidden2 = document.getElementById('hidden2');
+    if (hidden2.style.display == "none") {
+      hidden2.style.display = "block"
+    } else {
+      hidden2.style.display = "none"
+    }
+  }
 
+  function reveal3() {
+    let hidden3 = document.getElementById('hidden3');
+    if (hidden3.style.display == "none") {
+      hidden3.style.display = "block"
+    } else {
+      hidden3.style.display = "none"
+    }
+  }
   
